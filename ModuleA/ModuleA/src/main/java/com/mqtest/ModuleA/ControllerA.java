9192e74e7abad87d978a0ad6a156aa4a6f19ad97
Lovlesh Chauhan <lovlesh.chauhan@getinsured.com>
2020-12-29 10:52:24 +0530

    Added queue

--- ModuleA/ModuleA/src/main/java/com/mqtest/ModuleA/ControllerA.java
+++ ModuleA/ModuleA/src/main/java/com/mqtest/ModuleA/ControllerA.java
@@ -1,13 +1,14 @@
 package com.mqtest.ModuleA;
 
+import javax.jms.Queue;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mqtest.ModuleA.service.ServiceA;
-import com.mqtest.ModuleHelper.RequestA;
 
 @RestController
 public class ControllerA {
